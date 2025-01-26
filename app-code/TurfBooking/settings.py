a991718171cd1a93976a18710194b923bd0f7810
Malhar Kauthale <malharkauthale2019@gmail.com>
2025-01-26 20:05:43 +0530

    Update settings.py

--- app-code/TurfBooking/settings.py
+++ app-code/TurfBooking/settings.py
@@ -136,8 +136,9 @@ USE_TZ = True
 STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "turf-app-deployment-/TurfBookingApp/static")
+    os.path.join(BASE_DIR, "TurfBookingApp/static")
 ]
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
